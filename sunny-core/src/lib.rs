e0017ec3923752483f1670bcd87a75211010d588
Martin Billinger <flkazemakase@gmail.com>
2020-07-25 09:00:18 +0200

    separate macro crate

--- sunny-core/src/lib.rs
+++ sunny-core/src/lib.rs
@@ -73,43 +73,6 @@ impl std::fmt::Debug for Scm {
     }
 }
 
-#[macro_export]
-macro_rules! scm {
-    ((lambda ($($param:ident),*) $($body:tt)+)) => {
-        Scm::func(|args: &[Scm]| match args {
-            [$($param),*] => { $(scm![$body]);+ }
-            _ => panic!("Incorrect arity")
-        })
-    };
-
-    ((quote ($first:tt $($rest:tt)*))) => {
-        Scm::pair(
-            scm![(quote $first)],
-            scm![(quote ($($rest)*))]
-        )
-    };
-
-    ((quote ())) => {
-        Scm::Nil
-    };
-
-    ((quote $name:ident)) => {
-        Scm::symbol(stringify!($name))
-    };
-
-    ((quote $x:expr)) => {
-        scm![$x]
-    };
-
-    (($func:tt $($arg:tt)*)) => {
-        scm![$func].invoke(&[$(scm![$arg]),*])
-     };
-
-    //($x:ident) => {($x).clone()};
-
-    ($x:expr) => {Scm::from($x)};
-}
-
 pub fn cons(args: &[Scm]) -> Scm {
     match args {
         [car, cdr] => Scm::pair(car.clone(), cdr.clone()),
@@ -136,62 +99,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn integer_constant() {
-        assert_eq!(scm![1], Scm::Int(1));
-    }
-
-    #[test]
-    fn lambda_simple() {
-        assert_eq!(scm![(lambda () 0)].invoke(&[]), Scm::Int(0));
-    }
-
-    #[test]
-    fn lambda_simple_sequence() {
-        assert_eq!(scm![(lambda () 1 2 3)].invoke(&[]), Scm::Int(3));
-    }
-
-    #[test]
-    fn lambda_identity() {
-        assert_eq!(scm![(lambda (x) x)].invoke(&[Scm::Int(1)]), Scm::Int(1));
-    }
-
-    #[test]
-    fn apply_lambda_simple() {
-        assert_eq!(scm![((lambda () 0))], Scm::Int(0));
-    }
-
-    #[test]
-    fn apply_lambda_identity() {
-        assert_eq!(scm![((lambda (x) x) 1)], Scm::Int(1));
-    }
-
-    #[test]
-    fn apply_binary_primitive() {
-        assert_eq!(scm![(add 1 2)], Scm::Int(3));
-    }
-
-    #[test]
-    fn apply_unary_primitive() {
-        assert_eq!(scm![(square 3)], Scm::Int(9));
-    }
-
-    #[test]
-    fn apply_abstraction() {
-        assert_eq!(scm![((lambda (x) (add x x)) 3)], Scm::Int(6));
-    }
-
-    #[test]
-    fn pair() {
-        assert_eq!(scm![(cons 1 2)], Scm::pair(1, 2));
-    }
-
-    #[test]
-    fn quote() {
-        assert_eq!(scm![(quote (1 2))], Scm::pair(1, Scm::pair(2, Scm::Nil)));
-    }
-
-    #[test]
-    fn symbol() {
-        assert_eq!(scm![(quote foo)], Scm::symbol("foo"));
+    fn it_works() {
+        assert_eq!(add(&[Scm::Int(2), Scm::Int(2)]), Scm::Int(4));
     }
 }
---
+++ sunny-macro/src/lib.rs
@@ -0,0 +1,105 @@
+#[macro_export]
+macro_rules! scm {
+    ((lambda ($($param:ident),*) $($body:tt)+)) => {
+        Scm::func(|args: &[Scm]| match args {
+            [$($param),*] => { $(scm![$body]);+ }
+            _ => panic!("Incorrect arity")
+        })
+    };
+
+    ((quote ($first:tt $($rest:tt)*))) => {
+        Scm::pair(
+            scm![(quote $first)],
+            scm![(quote ($($rest)*))]
+        )
+    };
+
+    ((quote ())) => {
+        Scm::Nil
+    };
+
+    ((quote $name:ident)) => {
+        Scm::symbol(stringify!($name))
+    };
+
+    ((quote $x:expr)) => {
+        scm![$x]
+    };
+
+    (($func:tt $($arg:tt)*)) => {
+        scm![$func].invoke(&[$(scm![$arg]),*])
+     };
+
+    //($x:ident) => {($x).clone()};
+
+    ($x:expr) => {Scm::from($x)};
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use sunny_core::Scm;
+    use sunny_core::{add, cons, square};
+
+    #[test]
+    fn integer_constant() {
+        assert_eq!(scm![1], Scm::Int(1));
+    }
+
+    #[test]
+    fn lambda_simple() {
+        assert_eq!(scm![(lambda () 0)].invoke(&[]), Scm::Int(0));
+    }
+
+    #[test]
+    fn lambda_simple_sequence() {
+        assert_eq!(scm![(lambda () 1 2 3)].invoke(&[]), Scm::Int(3));
+    }
+
+    #[test]
+    fn lambda_identity() {
+        assert_eq!(scm![(lambda (x) x)].invoke(&[Scm::Int(1)]), Scm::Int(1));
+    }
+
+    #[test]
+    fn apply_lambda_simple() {
+        assert_eq!(scm![((lambda () 0))], Scm::Int(0));
+    }
+
+    #[test]
+    fn apply_lambda_identity() {
+        assert_eq!(scm![((lambda (x) x) 1)], Scm::Int(1));
+    }
+
+    #[test]
+    fn apply_binary_primitive() {
+        assert_eq!(scm![(add 1 2)], Scm::Int(3));
+    }
+
+    #[test]
+    fn apply_unary_primitive() {
+        assert_eq!(scm![(square 3)], Scm::Int(9));
+    }
+
+    #[test]
+    fn apply_abstraction() {
+        assert_eq!(scm![((lambda (x) (add x x)) 3)], Scm::Int(6));
+    }
+
+    #[test]
+    fn pair() {
+        assert_eq!(scm![(cons 1 2)], Scm::pair(1, 2));
+    }
+
+    #[test]
+    fn quote() {
+        assert_eq!(scm![(quote (1 2))], Scm::pair(1, Scm::pair(2, Scm::Nil)));
+    }
+
+    #[test]
+    fn symbol() {
+        assert_eq!(scm![(quote foo)], Scm::symbol("foo"));
+    }
+}
