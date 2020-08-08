3d1677b492b9a0772abe4f8e442994eee64b4930
Martin Billinger <mbillingr@users.noreply.github.com>
2020-08-08 22:14:01 +0200

    top level definitions

--- sunny-rustify/build.rs
+++ sunny-rustify/build.rs
@@ -16,19 +16,22 @@ fn main() {
     cmd.stdin
         .as_mut()
         .unwrap()
-        .write_all(b"(display ((lambda (x) (+ x 2)) 40))")
+        .write_all(b"
+(define foo 40)
+(define (println x) (display x) (newline))
+(println ((lambda (x) (+ x 2)) foo))")
         .unwrap();
 
     let output = cmd.wait_with_output().unwrap();
-
-    if !output.status.success() {
-        panic!("{}", String::from_utf8(output.stderr).unwrap());
-    }
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("hello.rs");
 
     fs::write(&dest_path, output.stdout).unwrap();
+
+    if !output.status.success() {
+        panic!("{}", String::from_utf8(output.stderr).unwrap());
+    }
 
     let _ = Command::new("rustfmt")
         .arg(&dest_path)
--- sunny-rustify/src/main.rs
+++ sunny-rustify/src/main.rs
@@ -1,4 +1,5 @@
 use sunny_core::Scm;
+use std::cell::Cell;
 
 include!(concat!(env!("OUT_DIR"), "/hello.rs"));
 
@@ -9,10 +10,16 @@ mod scheme {
         use sunny_core::{self, Scm};
 
         thread_local! {pub static display: Cell<Scm> = Cell::new(Scm::func(_display))}
+        thread_local! {pub static newline: Cell<Scm> = Cell::new(Scm::func(_newline))}
         thread_local! {pub static _plus_: Cell<Scm> = Cell::new(Scm::func(sunny_core::add))}
 
         fn _display(args: &[Scm]) -> Scm {
-            println!("{:?}", args[0]);
+            print!("{:?}", args[0]);
+            Scm::Nil
+        }
+
+        fn _newline(_: &[Scm]) -> Scm {
+            println!();
             Scm::Nil
         }
     }
