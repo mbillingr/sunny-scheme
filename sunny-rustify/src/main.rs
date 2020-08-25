fad2e3128a529f9961e1e23da402fde46d099282
Martin Billinger <mbillingr@users.noreply.github.com>
2020-08-25 20:16:19 +0200

    add cxr library

--- sunny-rustify/src/main.rs
+++ sunny-rustify/src/main.rs
@@ -20,6 +20,46 @@ mod scheme {
         thread_local! {pub static pair_p: Mut<Scm> = Mut::new(Scm::func1(Scm::is_pair))}
     }
 
+    pub mod cxr {
+        use sunny_core::{Mut, Scm};
+
+        thread_local! {pub static caaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().car().unwrap()))}
+        thread_local! {pub static caadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().car().unwrap()))}
+        thread_local! {pub static cadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().car().unwrap()))}
+        thread_local! {pub static caddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().car().unwrap()))}
+        thread_local! {pub static cdaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cdr().unwrap()))}
+        thread_local! {pub static cdadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cdr().unwrap()))}
+        thread_local! {pub static cddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cdr().unwrap()))}
+        thread_local! {pub static cdddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cdr().unwrap()))}
+
+        thread_local! {pub static caaaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().caar().unwrap()))}
+        thread_local! {pub static caaadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().caar().unwrap()))}
+        thread_local! {pub static caadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().caar().unwrap()))}
+        thread_local! {pub static caaddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().caar().unwrap()))}
+        thread_local! {pub static cadaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cdar().unwrap()))}
+        thread_local! {pub static cadadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cdar().unwrap()))}
+        thread_local! {pub static caddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cdar().unwrap()))}
+        thread_local! {pub static cadddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cdar().unwrap()))}
+        thread_local! {pub static cdaaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cadr().unwrap()))}
+        thread_local! {pub static cdaadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cadr().unwrap()))}
+        thread_local! {pub static cdadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cadr().unwrap()))}
+        thread_local! {pub static cdaddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cadr().unwrap()))}
+        thread_local! {pub static cddaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cddr().unwrap()))}
+        thread_local! {pub static cddadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cddr().unwrap()))}
+        thread_local! {pub static cdddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cddr().unwrap()))}
+        thread_local! {pub static cddddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cddr().unwrap()))}
+    }
+
+    pub mod read {
+        use sunny_core::{self, Mut, Scm};
+
+        thread_local! {pub static read: Mut<Scm> = Mut::new(Scm::func(_read))}
+
+        fn _read(args: &[Scm]) -> Scm {
+            unimplemented!()
+        }
+    }
+
     pub mod write {
         use sunny_core::{self, Mut, Scm};
 
