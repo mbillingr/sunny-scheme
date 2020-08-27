37cf56686e4c8a37b2e32e732f0582fde529e61c
Martin Billinger <mbillingr@users.noreply.github.com>
2020-08-27 19:52:23 +0200

    cxr

--- sunny-rustify/src/main.rs
+++ sunny-rustify/src/main.rs
@@ -2,10 +2,16 @@ use sunny_core::{Mut, Scm, MEMORY_MODEL_KIND};
 
 include!(concat!(env!("OUT_DIR"), "/hello.rs"));
 
+macro_rules! pipe {
+    ($f:tt) =>  { $f };
+    ($f:tt $g:ident) => { |x| $g(&[$f(x)]) };
+    ($f:tt $g:ident $($rest:ident)*) => { pipe![ (|x| $g(&[$f(x)])) $($rest)*] };
+}
+
 #[allow(non_upper_case_globals)]
 mod scheme {
     pub mod base {
-        use sunny_core::{self, Mut, Scm};
+        use sunny_core::{self, Mut, Scm, car as _car, cdr as _cdr};
 
         thread_local! {pub static _e_: Mut<Scm> = Mut::new(Scm::func(sunny_core::is_numeq))}
         thread_local! {pub static _g_: Mut<Scm> = Mut::new(Scm::func(sunny_core::is_numgt))}
@@ -14,6 +20,10 @@ mod scheme {
         thread_local! {pub static _plus_: Mut<Scm> = Mut::new(Scm::func(sunny_core::add))}
         thread_local! {pub static car: Mut<Scm> = Mut::new(Scm::func(sunny_core::car))}
         thread_local! {pub static cdr: Mut<Scm> = Mut::new(Scm::func(sunny_core::cdr))}
+        thread_local! {pub static caar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car]))}
+        thread_local! {pub static cadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car]))}
+        thread_local! {pub static cdar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr]))}
+        thread_local! {pub static cddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr]))}
         thread_local! {pub static cons: Mut<Scm> = Mut::new(Scm::func(sunny_core::cons))}
         thread_local! {pub static eof_minus_object_p: Mut<Scm> = Mut::new(Scm::func1(Scm::is_eof))}
         thread_local! {pub static eq_p: Mut<Scm> = Mut::new(Scm::func(sunny_core::is_ptreq))}
@@ -23,33 +33,33 @@ mod scheme {
     }
 
     pub mod cxr {
-        use sunny_core::{Mut, Scm};
+        use sunny_core::{Mut, Scm, car as _car, cdr as _cdr};
 
-        thread_local! {pub static caaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().car().unwrap()))}
-        thread_local! {pub static caadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().car().unwrap()))}
-        thread_local! {pub static cadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().car().unwrap()))}
-        thread_local! {pub static caddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().car().unwrap()))}
-        thread_local! {pub static cdaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cdr().unwrap()))}
-        thread_local! {pub static cdadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cdr().unwrap()))}
-        thread_local! {pub static cddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cdr().unwrap()))}
-        thread_local! {pub static cdddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cdr().unwrap()))}
+        thread_local! {pub static caaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _car]))}
+        thread_local! {pub static caadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _car]))}
+        thread_local! {pub static cadar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _car]))}
+        thread_local! {pub static caddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _car]))}
+        thread_local! {pub static cdaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _cdr]))}
+        thread_local! {pub static cdadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _cdr]))}
+        thread_local! {pub static cddar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _cdr]))}
+        thread_local! {pub static cdddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _cdr]))}
 
-        thread_local! {pub static caaaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().caar().unwrap()))}
-        thread_local! {pub static caaadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().caar().unwrap()))}
-        thread_local! {pub static caadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().caar().unwrap()))}
-        thread_local! {pub static caaddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().caar().unwrap()))}
-        thread_local! {pub static cadaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cdar().unwrap()))}
-        thread_local! {pub static cadadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cdar().unwrap()))}
-        thread_local! {pub static caddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cdar().unwrap()))}
-        thread_local! {pub static cadddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cdar().unwrap()))}
-        thread_local! {pub static cdaaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cadr().unwrap()))}
-        thread_local! {pub static cdaadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cadr().unwrap()))}
-        thread_local! {pub static cdadar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cadr().unwrap()))}
-        thread_local! {pub static cdaddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cadr().unwrap()))}
-        thread_local! {pub static cddaar: Mut<Scm> = Mut::new(Scm::func1(|x| x.caar().unwrap().cddr().unwrap()))}
-        thread_local! {pub static cddadr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cdar().unwrap().cddr().unwrap()))}
-        thread_local! {pub static cdddar: Mut<Scm> = Mut::new(Scm::func1(|x| x.cadr().unwrap().cddr().unwrap()))}
-        thread_local! {pub static cddddr: Mut<Scm> = Mut::new(Scm::func1(|x| x.cddr().unwrap().cddr().unwrap()))}
+        thread_local! {pub static caaaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _car _car]))}
+        thread_local! {pub static caaadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _car _car]))}
+        thread_local! {pub static caadar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _car _car]))}
+        thread_local! {pub static caaddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _car _car]))}
+        thread_local! {pub static cadaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _cdr _car]))}
+        thread_local! {pub static cadadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _cdr _car]))}
+        thread_local! {pub static caddar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _cdr _car]))}
+        thread_local! {pub static cadddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _cdr _car]))}
+        thread_local! {pub static cdaaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _car _cdr]))}
+        thread_local! {pub static cdaadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _car _cdr]))}
+        thread_local! {pub static cdadar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _car _cdr]))}
+        thread_local! {pub static cdaddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _car _cdr]))}
+        thread_local! {pub static cddaar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _car _cdr _cdr]))}
+        thread_local! {pub static cddadr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _car _cdr _cdr]))}
+        thread_local! {pub static cdddar: Mut<Scm> = Mut::new(Scm::func(pipe![_car _cdr _cdr _cdr]))}
+        thread_local! {pub static cddddr: Mut<Scm> = Mut::new(Scm::func(pipe![_cdr _cdr _cdr _cdr]))}
     }
 
     pub mod read {
