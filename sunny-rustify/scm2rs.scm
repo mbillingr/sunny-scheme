e42f115b3a9032598ef697b8189b5cdd46d425c6
mbilling <mbillingr@users.noreply.github.com>
2020-09-02 13:12:14 +0200

    add modules to program

--- sunny-rustify/scm2rs.scm
+++ sunny-rustify/scm2rs.scm
@@ -1177,36 +1177,75 @@
 
 
 (define (rust-gen-modules port libs)
-  '(root (hello l)
-         (scheme (cxr l)
-                 (stuff l)))
-  (println port "// " libs)
+  (let ((module-tree (make-module-tree-node 'root)))
+    (for-each (lambda (lib)
+                (module-tree-insert! module-tree (car lib) (cdr lib)))
+              libs)
 
-  (let ((hierarchy (list '())))
-    (for-each (lambda (lib) (module-hierarchy-insert! (car lib) hierarchy))
-              libs)))
+    (rust-gen-module-tree-list port (module-tree-children module-tree))))
 
-(define (module-hierarchy-insert! lib hierarchy)
-  (cond ((hierarchy-empty? hierarchy)
-         (init-hierarchy! hierarchy lib))
-        ((eq? (hierarchy-first hierarchy) (car lib))
-         (module-hierarchy-insert! (cdr lib) (subhierarchy hierarchy)))
-        (else
-         (module-hierarchy-insert! lib (hierarchy-rest hierarchy)))))
 
-(define (hierarchy-empty? h)
-  (null? (cdr h)))
+(define (rust-gen-module-tree port node)
+  (println port "pub mod "
+                (module-tree-name node)
+                " {")
+  (if (module-tree-leaf? node)
+      ((module-tree-libobj node) 'gen-rust port)
+      (rust-gen-module-tree-list port (module-tree-children node)))
+  (println port "}"))
 
-(define (init-hierarchy! h lib)
-  (set-cdr! h (listify-lib lib)))
+(define (rust-gen-module-tree-list port nodes)
+  (for-each (lambda (child)
+              (rust-gen-module-tree port child))
+            nodes))
 
-(define (hierarchy-first h)
-  (car h))
 
-(define (listify-lib lib)
-  (if (null? lib)
-      '()
-      (list (car lib) (listify-lib (cdr lib)))))
+(define (make-module-tree-node name)
+  (cons name '()))
+
+(define (make-module-tree-leaf name lib)
+  (cons name lib))
+
+(define (module-tree-leaf? node)
+  (and (pair? node)
+       (symbol? (car node))
+       (not (null? (cdr node)))
+       (not (pair? (cdr node)))))
+
+(define (module-tree-name node)
+  (car node))
+
+(define (module-tree-children node)
+  (cdr node))
+
+(define (module-tree-libobj node)
+  (cdr node))
+
+(define (module-tree-set-children! node children)
+  (set-cdr! node children))
+
+(define (module-tree-find-child node name)
+  (if (module-tree-leaf? node)
+      (error "called (module-tree-find-child) on leaf node" name node))
+  (assq name (module-tree-children node)))
+
+(define (module-tree-append-child! node child)
+  (module-tree-set-children!
+    node
+    (cons child (module-tree-children node))))
+
+(define (module-tree-insert! tree libname libobj)
+  (if (null? libname)
+      (error "invalid insert"))
+  (let ((child (module-tree-find-child tree (car libname))))
+    (if child
+        (module-tree-insert! child (cdr libname) libobj)
+        (if (null? (cdr libname))
+            (module-tree-append-child! tree (make-module-tree-leaf (car libname) libobj))
+            (let ((new-node (make-module-tree-node (car libname))))
+              (module-tree-insert! new-node (cdr libname) libobj)
+              (module-tree-append-child! tree new-node))))))
+
 
 
 
