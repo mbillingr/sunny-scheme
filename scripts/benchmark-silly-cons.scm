86af254cb7a6a4c2f8b46f7f79f74276aecc57de
Martin Billinger <flkazemakase@gmail.com>
2021-04-08 14:38:02 +0200

    more benchmarks

---
+++ scripts/benchmark-silly-cons.scm
@@ -0,0 +1,14 @@
+(import (scheme base)
+        (sunny time))
+
+(define (cons-a-lot n l)
+  (if (= n 0)
+      'ok
+      (cons-a-lot (- n 1)
+                  (cons n l))))
+
+(display
+  (report-time
+    (timeit (lambda ()
+              (cons-a-lot 1000000 '())))))
+(newline)
