139d94df2df0a3c7b82d2fbb813f6b8751382fc5
Martin Billinger <flkazemakase@gmail.com>
2021-04-18 13:48:45 +0200

    implement sexpr traits for Scm

--- scm/src/scm/sexpr_impls.rs
+++ scm/src/scm/sexpr_impls.rs
@@ -1,5 +1,41 @@
 use crate::Scm;
+use sexpr_generics::core_traits::{MaybeBool, MaybeChar, MaybePair, Nullable};
 use sexpr_generics::cxr::CxR;
+
+impl Nullable for Scm {
+    fn is_null(&self) -> bool {
+        Scm::is_null(self)
+    }
+}
+
+impl MaybeBool for Scm {
+    fn to_bool(&self) -> Option<bool> {
+        Scm::as_bool(self)
+    }
+}
+
+impl MaybeChar for Scm {
+    fn to_char(&self) -> Option<char> {
+        unimplemented!()
+    }
+
+    fn to_ascii(&self) -> Option<u8> {
+        unimplemented!()
+    }
+}
+
+impl MaybePair for Scm {
+    type First = Scm;
+    type Second = Scm;
+
+    fn first(&self) -> Option<&Self::First> {
+        self.as_pair().map(|(car, _)| car)
+    }
+
+    fn second(&self) -> Option<&Self::First> {
+        self.as_pair().map(|(_, cdr)| cdr)
+    }
+}
 
 impl CxR for Scm {
     type Result = Self;
